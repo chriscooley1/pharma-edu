68c9b156ad97fcb45cea8c055fc6f5fb13ce7efc
Chris Cooley <ccooley@bonadmin.com>
2024-09-05 10:24:45 -0600

    Add styling and borders for Add Patient

--- frontend/src/pages/AddPatient/AddPatient.tsx
+++ frontend/src/pages/AddPatient/AddPatient.tsx
@@ -28,10 +28,8 @@ interface InsuranceInfo {
 const AddPatient: React.FC = () => {
   const navigate = useNavigate();
 
-  // Set initial editMode to true to enable editing by default
-  const [editMode, setEditMode] = useState(true); 
-  
-  // Initial empty state for patient details and insurance info
+  const [editMode, setEditMode] = useState(true);
+
   const initialPatientDetails = {
     id: null,
     first_name: "",
@@ -58,10 +56,9 @@ const AddPatient: React.FC = () => {
   const [insuranceInfo, setInsuranceInfo] = useState<InsuranceInfo>(initialInsuranceInfo);
 
   useEffect(() => {
-    // Reset both patient and insurance info when navigating to "Add New Patient"
     setPatientDetails(initialPatientDetails);
     setInsuranceInfo(initialInsuranceInfo);
-  }, []); // Empty dependency array ensures this runs only on component mount
+  }, []);
 
   const handlePatientChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -71,7 +68,7 @@ const AddPatient: React.FC = () => {
     }));
   };
 
-  const handleInsuranceChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInsuranceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setInsuranceInfo((prevDetails) => ({
       ...prevDetails,
@@ -81,27 +78,21 @@ const AddPatient: React.FC = () => {
 
   const handleSave = async () => {
     try {
-      // Merge insuranceInfo with patientDetails for the initial POST request
       const fullPatientDetails = {
         ...patientDetails,
-        ...insuranceInfo, // Include insurance fields in the initial patient creation
+        ...insuranceInfo,
       };
 
       await axios.post("http://localhost:8000/patients", fullPatientDetails);
-
       alert("Patient and insurance details saved successfully!");
-      setEditMode(false); // Disable edit mode after saving
 
-      // Add "saved" class to the save button for visual feedback
       const saveButton = document.querySelector(".save-button");
       saveButton?.classList.add("saved");
-
-      // Remove "saved" class after 3 seconds
       setTimeout(() => {
         saveButton?.classList.remove("saved");
       }, 3000);
 
-      navigate("/patientprofile"); // Redirect to the PatientProfile page after saving
+      navigate("/patientprofile");
     } catch (error) {
       console.error("Error saving patient and insurance details:", error);
       alert("Failed to save patient and insurance details.");
@@ -109,192 +100,199 @@ const AddPatient: React.FC = () => {
   };
 
   const toggleEditMode = () => {
-    setEditMode(!editMode); // Toggle edit mode
+    setEditMode(!editMode);
   };
 
   return (
-    <div className="add-new-patient-container">
-      <div className="pt-left-side">
-        <div>
-          <label htmlFor="patient-first-name">First Name</label>
-          <input
-            type="text"
-            name="first_name"
-            id="patient-first-name"
-            value={patientDetails.first_name || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-last-name">Last Name</label>
-          <input
-            type="text"
-            name="last_name"
-            id="patient-last-name"
-            value={patientDetails.last_name || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-date-of-birth">Date Of Birth</label>
-          <input
-            type="date"
-            name="date_of_birth"
-            id="patient-date-of-birth"
-            value={patientDetails.date_of_birth || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-phone-number">Phone Number</label>
-          <input
-            type="text"
-            name="phone_number"
-            id="patient-phone-number"
-            value={patientDetails.phone_number || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-street">Street</label>
-          <input
-            type="text"
-            name="street"
-            id="patient-street"
-            value={patientDetails.street || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-city">City</label>
-          <input
-            type="text"
-            name="city"
-            id="patient-city"
-            value={patientDetails.city || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-state">State</label>
-          <input
-            type="text"
-            name="state"
-            id="patient-state"
-            value={patientDetails.state || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-zipcode">Zipcode</label>
-          <input
-            type="text"
-            name="zipcode"
-            id="patient-zipcode"
-            value={patientDetails.zipcode || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-allergies">Allergies</label>
-          <input
-            type="text"
-            name="allergies"
-            id="patient-allergies"
-            value={patientDetails.allergies || ""}
-            onChange={handlePatientChange}
-            readOnly={!editMode}
-          />
+    <div className="patient-profile-container">
+      {/* Header row for Patient Name, Edit, and Save buttons */}
+      <div className="pt-header-row">
+        <h3>New Patient</h3>
+        <div className="pt-header-buttons">
+          <button type="button" className="edit-button" onClick={toggleEditMode}>
+            {editMode ? "Cancel" : "Edit"}
+          </button>
+          <button type="button" className="save-button" onClick={handleSave} disabled={!editMode}>
+            Save
+          </button>
         </div>
       </div>
 
-      <div className="separator"></div>
+      <div className="pt-content">
+        <div className="pt-profile-left-side">
+          <h3>General Information</h3>
+          <div>
+            <label htmlFor="patient-first-name">First Name</label>
+            <input
+              type="text"
+              name="first_name"
+              id="patient-first-name"
+              value={patientDetails.first_name || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-last-name">Last Name</label>
+            <input
+              type="text"
+              name="last_name"
+              id="patient-last-name"
+              value={patientDetails.last_name || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-date-of-birth">Date Of Birth</label>
+            <input
+              type="date"
+              name="date_of_birth"
+              id="patient-date-of-birth"
+              value={patientDetails.date_of_birth || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-phone-number">Phone Number</label>
+            <input
+              type="text"
+              name="phone_number"
+              id="patient-phone-number"
+              value={patientDetails.phone_number || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-street">Street</label>
+            <input
+              type="text"
+              name="street"
+              id="patient-street"
+              value={patientDetails.street || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-city">City</label>
+            <input
+              type="text"
+              name="city"
+              id="patient-city"
+              value={patientDetails.city || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-state">State</label>
+            <input
+              type="text"
+              name="state"
+              id="patient-state"
+              value={patientDetails.state || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-zipcode">Zipcode</label>
+            <input
+              type="text"
+              name="zipcode"
+              id="patient-zipcode"
+              value={patientDetails.zipcode || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-allergies">Allergies</label>
+            <input
+              type="text"
+              name="allergies"
+              id="patient-allergies"
+              value={patientDetails.allergies || ""}
+              onChange={handlePatientChange}
+              readOnly={!editMode}
+            />
+          </div>
+        </div>
 
-      <div className="pt-right-side">
-        <h3>Insurance Info</h3>
-        <div>
-          <label htmlFor="patient-insurance-name">Insurance Name</label>
-          <input
-            type="text"
-            name="insurance_name"
-            id="patient-insurance-name"
-            value={insuranceInfo.insurance_name || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-insurance-member-id">Insurance Member ID</label>
-          <input
-            type="text"
-            name="insurance_member_id"
-            id="patient-insurance-member-id"
-            value={insuranceInfo.insurance_member_id || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-insurance-group-number">Insurance Group Number</label>
-          <input
-            type="text"
-            name="insurance_group_number"
-            id="patient-insurance-group-number"
-            value={insuranceInfo.insurance_group_number || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-insurance-rx-bin">Insurance Rx Bin</label>
-          <input
-            type="text"
-            name="insurance_rx_bin"
-            id="patient-insurance-rx-bin"
-            value={insuranceInfo.insurance_rx_bin || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-insurance-rx-pcn">Insurance Rx Pcn</label>
-          <input
-            type="text"
-            name="insurance_rx_pcn"
-            id="patient-insurance-rx-pcn"
-            value={insuranceInfo.insurance_rx_pcn || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-        <div>
-          <label htmlFor="patient-insurance-person-code">Person Code</label>
-          <input
-            type="text"
-            name="insurance_person_code"
-            id="patient-insurance-person-code"
-            value={insuranceInfo.insurance_person_code || ""}
-            onChange={handleInsuranceChange}
-            readOnly={!editMode}
-          />
-        </div>
-      </div>
+        <div className="separator"></div>
 
-      <div className="button-group">
-        <button type="button" className="edit-button" onClick={toggleEditMode}>
-          {editMode ? "Cancel" : "Edit"}
-        </button>
-        <button type="button" className="save-button" onClick={handleSave} disabled={!editMode}>
-          Save
-        </button>
+        <div className="pt-profile-right-side">
+          <h3>Insurance Info</h3>
+          <div>
+            <label htmlFor="patient-insurance-name">Name</label>
+            <input
+              type="text"
+              name="insurance_name"
+              id="patient-insurance-name"
+              value={insuranceInfo.insurance_name || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-insurance-member-id">Member ID</label>
+            <input
+              type="text"
+              name="insurance_member_id"
+              id="patient-insurance-member-id"
+              value={insuranceInfo.insurance_member_id || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-insurance-group-number">Group Number</label>
+            <input
+              type="text"
+              name="insurance_group_number"
+              id="patient-insurance-group-number"
+              value={insuranceInfo.insurance_group_number || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-insurance-rx-bin">Rx Bin</label>
+            <input
+              type="text"
+              name="insurance_rx_bin"
+              id="patient-insurance-rx-bin"
+              value={insuranceInfo.insurance_rx_bin || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-insurance-rx-pcn">Rx Pcn</label>
+            <input
+              type="text"
+              name="insurance_rx_pcn"
+              id="patient-insurance-rx-pcn"
+              value={insuranceInfo.insurance_rx_pcn || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+          <div>
+            <label htmlFor="patient-insurance-person-code">Person Code</label>
+            <input
+              type="text"
+              name="insurance_person_code"
+              id="patient-insurance-person-code"
+              value={insuranceInfo.insurance_person_code || ""}
+              onChange={handleInsuranceChange}
+              readOnly={!editMode}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
