416e64076e8124a51f215842c4e412cc000e76a3
Chris Cooley <ccooley@bonadmin.com>
2024-09-19 11:18:00 -0600

    Add dockerfile and fix seed

--- backend/database-seed/delete_records.sql
+++ backend/database-seed/delete_records.sql
@@ -3,7 +3,8 @@ DELETE FROM Patient;
 DELETE FROM Prescriber;
 DELETE FROM RxItem;
 
-ALTER SEQUENCE patients_id_seq RESTART WITH 1;
+-- Reset the sequences
+ALTER SEQUENCE patient_id_seq RESTART WITH 1;
 ALTER SEQUENCE prescriber_id_seq RESTART WITH 1;
 ALTER SEQUENCE rxitem_id_seq RESTART WITH 1;
 ALTER SEQUENCE prescription_rx_number_seq RESTART WITH 1;
